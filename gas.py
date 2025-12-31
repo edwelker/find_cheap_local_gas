ebfe08d426f1a60911ce4dc662c0edc2886b0856
Eddie Welker <eddie.welker@gmail.com>
2025-12-31 18:11:54 -0500

    wip

--- gas.py
+++ gas.py
@@ -232,6 +232,7 @@ def scrape_gasbuddy(region_config, headless=False):
         options.add_argument("--headless")
         options.add_argument("--no-sandbox")
         options.add_argument("--disable-dev-shm-usage")
+        options.add_argument("--disable-gpu")
         options.add_argument("--window-size=1920,1080")
     else:
         options.add_argument("--start-maximized")
